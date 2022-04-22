b006cb7069191e9fdf25faab57537c95c8a5f38b
Seon <public.seon@gmail.com>
2022-04-22 13:15:02 +0900

    feat: A Needle in the Haystack

---
+++ codewars/a-needle-in-the-haystack/index.js
@@ -0,0 +1,6 @@
+function findNeedle(haystack) {
+    // your code here
+    return `found the needle at position ${haystack.indexOf('needle')}`;
+}
+
+console.log(findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']));
