6de0e368e6e9cdad56ac01089266008496d53658
Seon <public.seon@gmail.com>
2022-04-22 13:48:56 +0900

    feat: Sum of positive

---
+++ codewars/sum-of-positive/index.js
@@ -0,0 +1,7 @@
+const positiveSum = (arr) => arr.reduce((a, b) => a + (b >= 0 ? b : 0), 0);
+
+console.log(positiveSum([1, 2, 3, 4, 5]));
+console.log(positiveSum([1, -2, 3, 4, 5]));
+console.log(positiveSum([]));
+console.log(positiveSum([-1, -2, -3, -4, -5]));
+console.log(positiveSum([-1, 2, 3, 4, -5]));
