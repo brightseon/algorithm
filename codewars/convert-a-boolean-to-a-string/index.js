bb3a227d436cd0ac2d5d82e165715a2d9392f913
Seon <public.seon@gmail.com>
2022-04-22 12:48:58 +0900

    feat: Convert a Boolean to a String

---
+++ codewars/convert-a-boolean-to-a-string/index.js
@@ -0,0 +1,10 @@
+function booleanToString(b) {
+    return b ? 'true' : 'false';
+}
+
+// ---------------------------------
+// function booleanToString(b) {
+//     return b.toString();
+// }
+
+console.log(booleanToString(true));
