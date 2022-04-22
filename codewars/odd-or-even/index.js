11466e807dd393648c8b89f735a535447d882eb9
Seon <public.seon@gmail.com>
2022-04-22 14:02:39 +0900

    feat: Odd or Even?

---
+++ codewars/odd-or-even/index.js
@@ -0,0 +1,7 @@
+function oddOrEven(array) {
+    return array.reduce((a, b) => a + b, 0) % 2 === 0 ? 'even' : 'odd';
+}
+
+console.log(oddOrEven([0]));
+console.log(oddOrEven([0, 1, 4]));
+console.log(oddOrEven([0, -1, -5]));
