3a3854b5d39e32836b84c38a84c982064a6d9b06
Seon <public.seon@gmail.com>
2022-04-27 12:53:50 +0900

    feat: Convert a String to a Number!

---
+++ codewars/convert-a-string-to-a-number/index.js
@@ -0,0 +1,6 @@
+const stringToNumber = str => parseInt(str);
+
+console.log(stringToNumber('1234'));
+console.log(stringToNumber('605'));
+console.log(stringToNumber('1405'));
+console.log(stringToNumber('-7'));
