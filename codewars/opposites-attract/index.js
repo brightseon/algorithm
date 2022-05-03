f06aee35be28021bfa2051c165caa479c2ae11ee
Seon <public.seon@gmail.com>
2022-05-03 11:03:55 +0900

    refactor: Opposites Attract

--- codewars/opposites-attract/index.js
+++ codewars/opposites-attract/index.js
@@ -1,5 +1,5 @@
 function lovefunc(flower1, flower2) {
-    return [flower1, flower2].reduce((a, b) => a + b, 0) % 2 !== 0;
+    return !((flower1 + flower2) % 2 === 0);
 }
 
 console.log(lovefunc(1, 4));
