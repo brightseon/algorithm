a981f276e712ecb7e1ae3181405c0ac99468ab9a
Seon <public.seon@gmail.com>
2022-05-04 08:30:35 +0900

    feat: The highest profit wins!

---
+++ codewars/the-highest-profit-wins/index.js
@@ -0,0 +1,8 @@
+function minMax(arr) {
+    return [Math.min(...arr), Math.max(...arr)];
+}
+
+console.log(minMax([1, 2, 3, 4, 5]));
+console.log(minMax([2334454, 5]));
+console.log(minMax([1]));
+console.log(minMax([5]));
