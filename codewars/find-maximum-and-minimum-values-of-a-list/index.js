96ed511d3d32d72870f1c359091335f09c0d05d1
Seon <public.seon@gmail.com>
2022-05-03 11:15:57 +0900

    refactor: Find Maximum and Minimum Values of a List

--- codewars/find-maximum-and-minimum-values-of-a-list/index.js
+++ codewars/find-maximum-and-minimum-values-of-a-list/index.js
@@ -1,11 +1,9 @@
 var min = function (list) {
-    list.sort((a, b) => a - b);
-    return list[0];
+    return Math.min(...list);
 };
 
 var max = function (list) {
-    list.sort((a, b) => b - a);
-    return list[0];
+    return Math.max(...list);
 };
 
 console.log(min([-52, 56, 30, 29, -54, 0, -110]));
