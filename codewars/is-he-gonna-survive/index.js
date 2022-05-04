7902dc8f3dd0e138b3888f0088fd13d5f6cca16c
Seon <public.seon@gmail.com>
2022-05-04 12:05:54 +0900

    feat: Is he gonna survive?

---
+++ codewars/is-he-gonna-survive/index.js
@@ -0,0 +1,10 @@
+function hero(bullets, dragons) {
+    return dragons * 2 <= bullets;
+}
+
+console.log(hero(10, 5));
+console.log(hero(7, 4));
+console.log(hero(4, 5));
+console.log(hero(100, 40));
+console.log(hero(1500, 751));
+console.log(hero(0, 1));
