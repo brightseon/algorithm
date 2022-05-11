5d33e5da1a07d7a46ead79634149d09a3b8e4af9
Seon <public.seon@gmail.com>
2022-05-11 13:27:57 +0900

    feat: Sentence Smash

---
+++ codewars/sentence-smash/index.js
@@ -0,0 +1,7 @@
+function smash(words) {
+    return words.join(' ');
+}
+
+console.log(smash(['hello', 'world']));
+console.log(smash(['hello', 'amazing', 'world']));
+console.log(smash(['this', 'is', 'a', 'really', 'long', 'sentence']));
