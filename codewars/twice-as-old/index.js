e326626879a8215b971b5a3d0fc6f2eade7e9f01
Seon <public.seon@gmail.com>
2022-05-18 14:44:11 +0900

    feat: Twice as old

---
+++ codewars/twice-as-old/index.js
@@ -0,0 +1,9 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs(dadYearsOld - 2 * sonYearsOld);
+}
+
+console.log(twiceAsOld(36, 7));
+console.log(twiceAsOld(55, 30));
+console.log(twiceAsOld(42, 21));
+console.log(twiceAsOld(22, 1));
+console.log(twiceAsOld(29, 0));
