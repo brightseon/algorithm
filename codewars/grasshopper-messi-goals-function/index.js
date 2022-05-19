fb1ed2e7e38a49b7d8648850ba37253fda671e57
Seon <public.seon@gmail.com>
2022-05-19 16:04:03 +0900

    feat: Grasshopper - Messi goals function

---
+++ codewars/grasshopper-messi-goals-function/index.js
@@ -0,0 +1,6 @@
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+console.log(goals(0, 0, 0));
+console.log(goals(43, 10, 5));
