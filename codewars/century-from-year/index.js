ba6ef07822f77f0440ec020ef2a198ca87f9c877
Seon <public.seon@gmail.com>
2022-05-19 12:55:11 +0900

    feat: Century From Year

---
+++ codewars/century-from-year/index.js
@@ -0,0 +1,9 @@
+function century(year) {
+    return Math.ceil(year / 100);
+}
+
+console.log(century(1705));
+console.log(century(1900));
+console.log(century(1601));
+console.log(century(2000));
+console.log(century(89));
