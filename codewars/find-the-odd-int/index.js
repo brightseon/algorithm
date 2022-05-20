18c3066aab0fa7b1cda5827e6a6371ec4def5d0c
Seon <public.seon@gmail.com>
2022-05-20 09:55:51 +0900

    feat: Find the odd int

---
+++ codewars/find-the-odd-int/index.js
@@ -0,0 +1,17 @@
+function findOdd(A) {
+    const obj = A.reduce(
+        (a, b) => ({
+            ...a,
+            [b]: (a[b] || 0) + 1
+        }),
+        {}
+    );
+    return +Object.keys(obj).filter((o) => obj[o] % 2 !== 0)[0];
+}
+
+console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]));
+console.log(findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]));
+console.log(findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]));
+console.log(findOdd([10]));
+console.log(findOdd([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1]));
+console.log(findOdd([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10]));
