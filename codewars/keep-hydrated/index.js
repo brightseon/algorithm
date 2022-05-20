6f732c056f31cb7d3eeabaddc306ae3158a21be3
Seon <public.seon@gmail.com>
2022-05-20 12:08:45 +0900

    feat: Keep Hydrated!

---
+++ codewars/keep-hydrated/index.js
@@ -0,0 +1,11 @@
+function litres(time) {
+    return Math.floor(time * 0.5);
+}
+
+console.log(litres(2));
+console.log(litres(1.4));
+console.log(litres(12.3));
+console.log(litres(0.82));
+console.log(litres(11.8));
+console.log(litres(1787));
+console.log(litres(0));
