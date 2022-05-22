66a8865e8ff7fb84e676c362ec44a82cbfbaef4c
Seon <public.seon@gmail.com>
2022-05-23 08:40:49 +0900

    feat: Grasshopper - Check for factor

---
+++ codewars/grasshopper-check-for-factor/index.js
@@ -0,0 +1,13 @@
+function checkForFactor(base, factor) {
+    return base % factor === 0;
+}
+
+console.log(checkForFactor(10, 2));
+console.log(checkForFactor(63, 7));
+console.log(checkForFactor(2450, 5));
+console.log(checkForFactor(24612, 3));
+
+console.log(checkForFactor(9, 2));
+console.log(checkForFactor(653, 7));
+console.log(checkForFactor(2453, 5));
+console.log(checkForFactor(24617, 3));
