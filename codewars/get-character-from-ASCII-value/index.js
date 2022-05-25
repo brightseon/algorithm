9990cf99bbf28e4502cf87171d9a7b9e6ed24ba3
Seon <public.seon@gmail.com>
2022-05-25 13:03:03 +0900

    feat: get character from ASCII Value

---
+++ codewars/get-character-from-ASCII-value/index.js
@@ -0,0 +1,15 @@
+function getChar(c) {
+    return String.fromCharCode(c);
+}
+
+console.log(getChar(55));
+console.log(getChar(56));
+console.log(getChar(57));
+console.log(getChar(58));
+console.log(getChar(59));
+console.log(getChar(60));
+console.log(getChar(61));
+console.log(getChar(62));
+console.log(getChar(63));
+console.log(getChar(64));
+console.log(getChar(65));
