267196c95ac71cb16e501a63c43043b7726f7b7d
Seon <public.seon@gmail.com>
2022-05-25 13:00:38 +0900

    feat: You only need one - Beginner

---
+++ codewars/you-only-need-one-beginner/index.js
@@ -0,0 +1,8 @@
+function check(a, x) {
+    return a.indexOf(x) !== -1;
+}
+
+console.log(check([66, 101], 66));
+console.log(check([101, 45, 75, 105, 99, 107], 107));
+console.log(check(['t', 'e', 's', 't'], 'e'));
+console.log(check(['what', 'a', 'great', 'kata'], 'kat'));
