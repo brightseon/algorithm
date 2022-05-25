ca6c192170a3f2da37062494ed30fcaa04e47879
Seon <public.seon@gmail.com>
2022-05-25 14:14:31 +0900

    feat: Switch it Up!

---
+++ codewars/switch-it-up/index.js
@@ -0,0 +1,7 @@
+function switchItUp(number) {
+    return ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'][number];
+}
+
+console.log(switchItUp(1));
+console.log(switchItUp(3));
+console.log(switchItUp(5));
