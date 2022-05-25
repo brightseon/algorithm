4e43bf03fff2ccc80c15ffa5c91ff4279fcd11be
Seon <public.seon@gmail.com>
2022-05-25 12:39:30 +0900

    feat: Hex to Decimal

---
+++ codewars/hex-to-decimal/index.js
@@ -0,0 +1,9 @@
+function hexToDec(hexString) {
+    return parseInt(hexString, 16);
+}
+
+console.log(hexToDec('1'));
+console.log(hexToDec('a'));
+console.log(hexToDec('10'));
+console.log(hexToDec('FF'));
+console.log(hexToDec('-C'));
