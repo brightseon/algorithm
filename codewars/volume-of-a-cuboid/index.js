3eafe09b9e3c518447a73753144419f0135f8665
Seon <public.seon@gmail.com>
2022-05-26 09:04:53 +0900

    feat: Volume of a Cuboid

---
+++ codewars/volume-of-a-cuboid/index.js
@@ -0,0 +1,8 @@
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+        return width * height * length;
+    }
+}
+
+console.log(Kata.getVolumeOfCuboid(1, 2, 2));
+console.log(Kata.getVolumeOfCuboid(6.3, 2, 5));
