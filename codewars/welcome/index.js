703607dd5af2e7c9de98c5dc33c9f341f32345b8
Seon <public.seon@gmail.com>
2022-05-26 12:05:10 +0900

    feat: Welcome!

---
+++ codewars/welcome/index.js
@@ -0,0 +1,27 @@
+function greet(language) {
+    const GREETS = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+    };
+
+    return GREETS[language] || GREETS.english;
+}
+
+console.log(greet('english'));
+console.log(greet('dutch'));
+console.log(greet('IP_ADDRESS_INVALID'));
