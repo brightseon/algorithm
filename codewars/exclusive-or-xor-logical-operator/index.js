9421345cf89c4a9d02299dc6e0c721cf1d938dcc
Seon <public.seon@gmail.com>
2022-06-02 08:41:30 +0900

    feat: Exclusive "or" (xor) Logical Operator

---
+++ codewars/exclusive-or-xor-logical-operator/index.js
@@ -0,0 +1,9 @@
+function xor(a, b) {
+    return (a ^ b) === 1;
+}
+
+console.log(xor(false, false));
+console.log(xor(true, false));
+console.log(xor(false, true));
+console.log(xor(true, true));
+console.log(xor(true, true));
