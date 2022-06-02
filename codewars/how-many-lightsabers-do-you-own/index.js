215078beff7b55f1cf0d390554c420b5becb7366
Seon <public.seon@gmail.com>
2022-06-02 15:55:46 +0900

    feat: How many lightsabers do you own?

---
+++ codewars/how-many-lightsabers-do-you-own/index.js
@@ -0,0 +1,7 @@
+function howManyLightsabersDoYouOwn(name) {
+    return name === 'Zach' ? 18 : 0;
+}
+
+console.log(howManyLightsabersDoYouOwn());
+console.log(howManyLightsabersDoYouOwn('Adam'));
+console.log(howManyLightsabersDoYouOwn('Zach'));
