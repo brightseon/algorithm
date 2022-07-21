a39eb1c723a3acd1e02a3a7365ccd043d26f04dd
Seon <public.seon@gmail.com>
2022-07-22 08:32:20 +0900

    feat: Alan Partridge II - Apple Turnover

---
+++ codewars/alan-partridge-II-apple-turnover/index.js
@@ -0,0 +1,8 @@
+function apple(x) {
+    return Math.pow(x, 2) > 1000
+        ? `It's hotter than the sun!!`
+        : 'Help yourself to a honeycomb Yorkie for the glovebox.';
+}
+
+console.log(apple('50'));
+console.log(apple(4));
