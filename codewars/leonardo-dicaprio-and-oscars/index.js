f43398bac78994d2860c13c7c7395e55bd9a4188
Seon <public.seon@gmail.com>
2022-07-25 13:34:08 +0900

    feat: Leonardo Dicaprio and Oscars

---
+++ codewars/leonardo-dicaprio-and-oscars/index.js
@@ -0,0 +1,13 @@
+function leo(oscar) {
+    if (oscar === 86) return 'Not even for Wolf of wallstreet?!';
+    if (oscar === 88) return 'Leo finally won the oscar! Leo is happy';
+    if (oscar >= 89) return 'Leo got one already!';
+
+    return 'When will you give Leo an Oscar?';
+}
+
+console.log(leo(89));
+console.log(leo(88));
+console.log(leo(87));
+console.log(leo(86));
+console.log(leo(12));
