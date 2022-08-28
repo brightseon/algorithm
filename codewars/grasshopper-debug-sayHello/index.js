cb1f1c5aea00b320ca14bd6847e01376f142a6a1
Seon <public.seon@gmail.com>
2022-08-29 08:34:29 +0900

    feat: Grasshopper - Debug sayHello

---
+++ codewars/grasshopper-debug-sayHello/index.js
@@ -0,0 +1,8 @@
+function sayHello(name) {
+    return `Hello, ${name}`;
+}
+
+console.log(sayHello('Mr. Spock'));
+console.log(sayHello('Captain Kirk'));
+console.log(sayHello('Liutenant Uhura'));
+console.log(sayHello('Dr. McCoy'));
