82f22525d55a608f9e50fc16eb178fe7603cad0f
Seon <public.seon@gmail.com>
2022-10-13 09:04:24 +0900

    refactor: Detect Pangram

--- codewars/detect-pangram/index.js
+++ codewars/detect-pangram/index.js
@@ -1,50 +1,6 @@
 function isPangram(string) {
-    const alphabet = [
-        'a',
-        'b',
-        'c',
-        'd',
-        'e',
-        'f',
-        'g',
-        'h',
-        'i',
-        'j',
-        'k',
-        'l',
-        'm',
-        'n',
-        'o',
-        'p',
-        'q',
-        'r',
-        's',
-        't',
-        'u',
-        'v',
-        'w',
-        'x',
-        'y',
-        'z'
-    ];
-    const str = string.replace(/[0-9.]/g, '').toLowerCase();
-    let result = true;
-
-    for (let i = 0; i < alphabet.length; i++) {
-        if (str.indexOf(alphabet[i]) >= 0) continue;
-
-        result = false;
-        break;
-    }
-
-    return result;
+    return new Set(string.toLowerCase().match(/[a-z]/g)).size === 26;
 }
-
-// --------------------------------------------------------------------------------------------------------
-// function isPangram(string) {
-//     const str = string.replace(/[0-9.]/g, '').toLowerCase();
-//     return 'abcdefghijklmnopqrstuvwxyz'.split('').every((s) => str.indexOf(s) !== -1);
-// }
 
 console.log(isPangram('The quick brown fox jumps over the lazy dog.'));
 console.log(isPangram('This is not a pangram.'));
