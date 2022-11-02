9435487ddc6686606271a1101a49857783069701
Seon <public.seon@gmail.com>
2022-11-02 09:35:46 +0900

    feat: 2236. Root Equals Sum of Children

---
+++ leetcode/2235-add-two-integers/2236-root-equals-sum-of-children/index.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var checkTree = function (root) {
+    return root.val === root.left.val + root.right.val;
+};
+
+console.log(checkTree([10, 4, 6]));
+console.log(checkTree([5, 3, 1]));
