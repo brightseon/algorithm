b17b5a50c5103f7c3c1d70159e8ff8d79818fae5
Seon <public.seon@gmail.com>
2023-02-08 08:51:45 +0900

    feat: Factorial

---
+++ codewars/factorial/README.md
@@ -0,0 +1,3 @@
+# Factorial
+
+reference: https://www.codewars.com/kata/57a049e253ba33ac5e000212
---
+++ codewars/factorial/main.py
@@ -0,0 +1,12 @@
+def factorial(n):
+  pass
+
+
+print(factorial(0))
+print(factorial(1))
+print(factorial(2))
+print(factorial(3))
+print(factorial(4))
+print(factorial(5))
+print(factorial(6))
+print(factorial(7))
